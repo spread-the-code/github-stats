bfb8b932287d4e14975815c24b9db3dd4606e614
shayanypn <shayanypn@gmail.com>
2020-06-08 20:54:44 +0430

    Updates

---
+++ precache-manifest.33264663b81c5d06b0019b8a14aed314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c800c9ec6850a848ee80cf7d4c415fea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ff9e51d82295426ac74",
+    "url": "./static/js/2.4af02635.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.4af02635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5ff4797daebe231147",
+    "url": "./static/js/main.1de015a4.chunk.js"
+  },
+  {
+    "revision": "3cf004a6f2171998cea4",
+    "url": "./static/js/runtime-main.a4631097.js"
+  }
+]);
